60268b04c84cd211b3c37350798691843213e7bf
CS671 <47447396+CS671@users.noreply.github.com>
2019-04-23 20:17:46 +0530

    Update Assignment_q2.py

--- Assignment_q2.py
+++ Assignment_q2.py
@@ -8,21 +8,46 @@ from tensorflow import Tensor
 
 I = tf.eye(100, dtype= tf.float64)
 I = I
+output="/home/supriyo/practice/"
 
-mass = np.load("/home/souvik/Desktop/DEEP_learning/q2_input/masses.npy")                            # 100 * 1
-Initial_position = np.load("/home/souvik/Desktop/DEEP_learning/q2_input/positions.npy")              # 100 * 2
-Initial_velocity = np.load("/home/souvik/Desktop/DEEP_learning/q2_input/velocities.npy")              # 100 * 2
+mass = np.load("/home/supriyo/practice/q2_input/masses.npy")                            # 100 * 1
+position = np.load("/home/supriyo/practice/q2_input/positions.npy")              # 100 * 2
+velocity = np.load("/home/supriyo/practice/q2_input/velocities.npy")              # 100 * 2
 
-mass = tf.convert_to_tensor(mass, dtype=tf.float64)
-Initial_position = tf.convert_to_tensor(Initial_position, dtype=tf.float64)
-Initial_velocity = tf.convert_to_tensor(Initial_velocity, dtype=tf.float64)
+
+#mass=tf.constant(mass,tf.float64)
+#mass = tf.convert_to_tensor(mass, dtype=tf.float64)
+#Initial_position = tf.convert_to_tensor(Initial_position, dtype=tf.float64)
+#Initial_velocity = tf.convert_to_tensor(Initial_velocity, dtype=tf.float64)
 
 G = tf.dtypes.cast(tf.constant(6.67 * (10 ** 5)), tf.float64)                 # Gravitational constant
-Th = tf.dtypes.cast(tf.constant(0.1), tf.float64)                             # Threshold distance
+Th = 0.1                             # Threshold distance
 del_t = tf.constant(10 ** (-4), tf.float64)                   # Time Step
 
 #pos = tf.placeholder(tf.float64,None)
 #vel = tf.placeholder(tf.float64,None)
+
+n=mass.shape[0]
+
+
+#various place holder that will take values later on
+x=tf.placeholder(tf.float64,[n,2],name="x")
+m=tf.placeholder(tf.float64,[n,1],name="m")
+v=tf.placeholder(tf.float64,[n,2],name="v")
+
+
+
+
+#this function check that a pair of object exits having distance betwen them is less than threshold    
+def check_pair(x,Th):
+    n=x.shape[0]
+    for i in range(n):
+        for j in range(n):
+            if i!=j:
+                dist=pow(pow(x[j][0]-x[i][0],2)+pow(x[j][1]-x[i][1],2),0.5)
+                if dist<= Th:
+                    return 1,i,j
+    return 0,i,j
 
 
 def _rel_dist_x_(position):
@@ -78,33 +103,49 @@ def _velocity_update_(velocity, acceleration):
     v_t_update = tf.math.add(velocity, a_t)
     return v_t_update
 
-
-def _update_(position, velocity, acceleration):
-    position = _position_update_(position, velocity, acceleration)
-    velocity = _velocity_update_(velocity, acceleration)
-    return  position, velocity
+a=_acceleration_(x)
+v_final=_velocity_update_(v, a)
+x_final=_position_update_(x, v, a)
 
 
-def _print_(position, velocity):
-    print((position))
-    print((velocity))
+
+#def _update_(position, velocity, acceleration):
+#    position = _position_update_(position, velocity, acceleration)
+#    velocity = _velocity_update_(velocity, acceleration)
+#    return  position, velocity
+
+
+#def _print_(position, velocity):
+#    print((position))
+#    print((velocity))
 #print(sess.run(count))
 
-count=0
+
+
+#count=0
+#here we start the session to compute the final velocities and positions of particles
 with tf.Session() as sess:
+    writer = tf.summary.FileWriter("output_log", sess.graph)
+    a1=0;j=0
+    while(a1!=1):
+        fd={x:position,v:velocity,m:mass}
+        v_f=sess.run(v_final,feed_dict=fd)
+        x_f=sess.run(x_final,feed_dict=fd)
 
-    pos = Initial_position
-    vel = Initial_velocity
-    #for i in range(0):
-    relative_dist_3 = _rel_dist_cube_(pos)
-    r_n = tf.math.pow(relative_dist_3, (1/3))
-    #tf.matrix_set_diag(r_n, 0.2)
-    accel = _acceleration_(pos)
-    for i in range(197):
-        pos = _position_update_(pos, vel, accel)
-        vel = _velocity_update_(vel, accel)
-        count=count+1
+        velocity=v_f
+        position=x_f
+        a1,b1,c1=check_pair(position,Th)
+        j+=1
+        if a1==1:
+            #print(j,b1,c1)
+            np.save(output+"positions.npy",position)
+	    #print(sess.run('final_position'+str(x1)))
+            np.save(output+"velocities.npy",velocity)
+            #print(sess.run('final_velocity'+str(v1)))
+            break;
+    # for i in range(n):
+    #     print(i,x1[i])
+    # print(v_f)
+    writer.close()
 
-        r=tf.where(tf.reduce_min(r_n) <= Th,x=print(sess.run(pos)),y=None)
-        #print(sess.run(pos))
-
+print("In",j,"th iteration distance between ",b1,"th and ",c1,"th particle becomes less than ",Th)
